eba95c17a0c8eef0f2a52d59f611c7630f01c0ba
abdulla <together8564@gmail.com>
2021-09-14 22:25:37 +0500

    hrm :: added zuul api gateway

---
+++ zuul-gateway/src/test/java/uz/hrm/zuul/gateway/ZuulGatewayApplicationTests.java
@@ -0,0 +1,11 @@
+package uz.hrm.zuul.gateway;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatewayApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
